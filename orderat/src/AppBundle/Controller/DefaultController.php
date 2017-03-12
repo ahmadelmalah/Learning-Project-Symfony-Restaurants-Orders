03c54499df687f3bb213f82a6fd552ba3fca9e19
ahmadelmalah <ahmedelmalah@hotmail.com>
2017-03-12 20:29:34 +0200

    Configuring access control

--- orderat/src/AppBundle/Controller/DefaultController.php
+++ orderat/src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -17,5 +18,19 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    /**
+     * @Route("/test", name="admin")
+     */
+    public function testAction(Request $request)
+    {
+      if ($this->isGranted('ROLE_USER') == false) {
+          return new Response( 'Who Are You?' );
+      }
+        $user = $this->getDoctrine()
+        ->getRepository('AppBundle:User')
+        ->find(1);
+        return new Response($user->getUsername());
     }
 }
