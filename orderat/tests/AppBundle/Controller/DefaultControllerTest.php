fb0668b3c13ef607bee4c2847489a00e40c1a12d
ahmadelmalah <ahmedelmalah@hotmail.com>
2017-03-30 21:50:12 +0200

    Functional test: user authontication

--- orderat/tests/AppBundle/Controller/DefaultControllerTest.php
+++ orderat/tests/AppBundle/Controller/DefaultControllerTest.php
@@ -10,9 +10,9 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome', $crawler->filter('bs-example-navbar-collapse-')->text());
+        $crawler = $client->request('GET', '/admin');
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $crawler = $client->request('GET', '/active');
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
     }
 }
