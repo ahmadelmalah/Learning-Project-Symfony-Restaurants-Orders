6cf828745ef5db730f669e0fa624f4ff4c1e59ba
ahmadelmalah <ahmedelmalah@hotmail.com>
2017-04-26 17:18:14 +0200

    finishing ajaxing all pages

---
+++ orderat/web/js/orderat.order-parser.js
@@ -0,0 +1,34 @@
+function jsonEscape(str){
+    return str.replace(/\n/g, '').replace(/\t/g, '');
+}
+
+function getOrderTemplate(order){
+  var orderTemplate = `
+  <div class="panel panel-success">
+    <div class="panel-heading">
+      <h3 class="panel-title"><b>${order["restaurant"]}</b> (created by: ${order["creator"]})</h3>
+    </div>
+    <div class="panel-body">
+      ${order["text"]}
+    </div>
+  </div>
+  `;
+  return orderTemplate;
+}
+
+function getAllOrdersTemplate(orders){
+  var allOrdersTemplate = '';
+  for (var key in orders) {
+       var order = orders[key];
+       allOrdersTemplate += getOrderTemplate(order);
+   }
+   return allOrdersTemplate;
+}
+
+function getOrdersFromResponse(response){
+  return JSON.parse(jsonEscape(response["orders"]));
+}
+
+function getResponseFromResponseStream(responseStream){
+  return JSON.parse(jsonEscape(responseStream));
+}
